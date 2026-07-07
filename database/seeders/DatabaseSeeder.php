37a7f04df12c75b23b13d3b93f624a4a59e010d9
Shomik Shahriar <151670863+Hapi-Guy@users.noreply.github.com>
2026-07-08 05:12:00 +0600

    postfactory&_seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,50 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+    // use WithoutModelEvents;
 
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        //gonna create a user before exec cat seeder or post seeder so that it don't throw error
+        $user = User::factory()->create([
+            'name' => 'DummyUser',
+            'email' => 'dummy@example.com',
         ]);
+
+
+        // to create categories and post into DB
+        $categories=[
+            'Artificial Intelligence',
+            'Machine Learning',
+            'Deep Learning',
+            'Neural Networks',
+            'Data Science',
+            'Big Data',
+            'Cloud Computing',
+            'Internet of Things',
+            'Cybersecurity',
+            'Blockchain'
+        ];
+        foreach($categories as $category){
+            Category::create([
+                'name' => $category,
+            ]);
+        }
+        Post::factory(100)->create();
+
+        // $this->call([
+        //     PostSeeder::class
+        // ]);
     }
 }
